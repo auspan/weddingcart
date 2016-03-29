074d63638124aceea9443f4083a1ca5a3b3a030c
Rajan Singh Rawat <rmprajan786@gmail.com>
2016-03-29 19:26:57 +0530

    calender issue Of editweddingform is solved

--- weddingcart/resources/views/pages/editweddingform.blade.php
+++ weddingcart/resources/views/pages/editweddingform.blade.php
@@ -19,9 +19,9 @@
 								{!! Form::open(['action'=>'WeddingController@update', 'class'=>'form-horizontal nobottommargin', 'method'=>'post', 'files'=>true]) !!}
                                		<div class="row">
                                                 <div class="input-daterange travel-date-group bottommargin-sm">
-                                                    <div class="col-md-4 divcenter">
+                                                    <div class="col-md-4 divcenter" style="position: relative; z-index: 10">
                                                         <label for="">Wedding Date</label>
-                                                        <input class="sm-form-control" name="wedding_date" placeholder="MM/DD/YYYY" type="text" value="{{ $wedding_date }}">
+                                                        <input class="sm-form-control" name="wedding_date" placeholder="MM/DD/YYYY" type="text"  id="weddate" value="{{ $wedding_date }}">
                                                     </div>
                                                         <input type="hidden" name="wed_date" class="form-control" value="wdt">
                                                     
