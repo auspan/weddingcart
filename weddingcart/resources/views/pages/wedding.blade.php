4a09bbc57c2a3c3f8dc877405d46e66bd7f2afc7
Akhleshrana <akhleshrana016@gmail.com>
2016-03-31 18:55:43 +0530

    change set counter

--- weddingcart/resources/views/pages/wedding.blade.php
+++ weddingcart/resources/views/pages/wedding.blade.php
@@ -2,6 +2,80 @@
 
     @section('content')
 
+        <section class="sectionmar" id="content">
+
+            <div class="content-wrap">
+            
+                <div class="container clearfix divcon">
+                    <div class="col_half">
+                        <img class="img-circle img-responsive" src="{{ asset('../uploads/' . $groom_image) }}" alt="Bryant Kellam" style="width: 150px; height: 150px; object-fit: cover;">
+                    </div>
+                    <div class="col_half col_last">
+                        <img class="img-circle img-responsive" src="{{ asset('../uploads/' . $bride_image) }}" alt="Bryant sdfg" style="width: 150px; height: 150px; object-fit: cover;">
+                    </div>
+                </div>
+
+                <div class="container clearfix">            
+                            
+                        <div class="center divcentr ">
+
+                            <div class="wedding-head clearfix">
+                              <div class="first-name divgrnm">{{ $groom_name }}</div>
+                               
+                                <div class="and">&amp;</div>
+                                <div class="last-name divgrnm">{{  $bride_name  }}</span></div>
+                            </div>
+            
+                            <div class="divider divider-short divider-center"><i class="icon-heart-empty"></i></div>
+            
+                            <p class="lead">Getting <strong>Hitched</strong> on:</p>
+            
+                             <div id="countdown-ex1" class="countdown countdown-large divcenter is-countdown divcount"><span class="countdown-row countdown-show4"><span class="countdown-section"><span class="countdown-amount" id="days"></span><span class="countdown-period">Days</span></span><span class="countdown-section"><span class="countdown-amount" id="hours"></span><span class="countdown-period">Hours</span></span><span class="countdown-section"><span class="countdown-amount" id="minutes"></span><span class="countdown-period">Minutes</span></span><span class="countdown-section"><span class="countdown-amount" id="seconds"></span><span class="countdown-period">Seconds</span></span></span>
+                             </div>
+                             <div id="dates">{{ $wedding_date }}</div>
+
+                           
+
+
+                            <div class="divider divider-short divider-center"><i class="icon-heart-empty"></i></div>
+                             
+                                <div class="first-name h3">
+                                    {{ $groom_name }} 
+                                </div>
+                                <div class="and h1">&amp;</div>
+                                <div class="last-name h3">
+                                    {{ $bride_name }}
+                                </div>
+                            
+                            <div class="center">
+                                <a href="{{ url('wedding/'.$UserId.'/edit') }}" class="button button-border button-rounded topmargin">Edit</a>
+
+                            </div>
+            
+                        </div>
+            
+            
+                       
+                                                
+                </div>
+                
+                <div class="divider divider-center"><a href="#" data-scrollto="#header"><i class="icon-chevron-up"></i></a></div>
+
+                <div class="center bottommargin-lg">
+
+                    <a href="#" class="button button-rounded button-xlarge">Wish List</a>
+                    <a href="#" class="button button-rounded button-xlarge">Invite</a>
+
+                </div>
+
+            </div>
+
+        </section><!-- #content end -->
+
+        @stop    @extends('app')
+
+    @section('content')
+
 		<section class="sectionmar" id="content">
 
 			<div class="content-wrap">
