eda26878f90542edcf20ba6734854a2faf9ae5eb
Rajan Singh Rawat <rmprajan786@gmail.com>
2016-04-11 11:33:03 +0530

    Corrected foreign key relationship with user_event_roles

--- weddingcart/database/migrations/2015_11_16_091645_add_foreign_keys_to_user_event_wishlist_items_table.php
+++ weddingcart/database/migrations/2015_11_16_091645_add_foreign_keys_to_user_event_wishlist_items_table.php
@@ -15,7 +15,7 @@ class AddForeignKeysToUserEventWishlistItemsTable extends Migration {
 		Schema::table('user_event_wishlist_items', function(Blueprint $table)
 		{
 			$table->foreign('product_id', 'fk_user_event_wishlist_items_products1')->references('id')->on('products')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-			$table->foreign('user_event_role_id', 'fk_user_event_wishlist_items_user_event_rols1')->references('id')->on('user_events')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+			$table->foreign('user_event_role_id', 'fk_user_event_wishlist_items_user_event_rols1')->references('id')->on('user_event_roles')->onUpdate('NO ACTION')->onDelete('NO ACTION');
 		});
 	}
 
