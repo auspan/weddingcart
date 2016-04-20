170281a71ac937ac4d21610369bc4e5167cf86af
Utkal <utkal.pande@gmail.com>
2016-04-20 13:17:14 +0530

    Corrected Merge Error

--- weddingcart/app/User.php
+++ weddingcart/app/User.php
@@ -61,6 +61,7 @@ class User extends Model implements AuthenticatableContract,
     public function contacts()  {
     
         return $this->hasMany('weddingcart\contact');
+        }
 
 
     public function isNew()
