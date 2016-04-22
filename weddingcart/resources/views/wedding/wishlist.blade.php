7ee2c3703b3c40b37ce50087fdcea1ea5ab26b32
Rajan Singh Rawat <rmprajan786@gmail.com>
2016-04-22 12:01:15 +0530

    Resolve wishlist slider problem in wishlist.blade.php

--- weddingcart/resources/views/wedding/wishlist.blade.php
+++ weddingcart/resources/views/wedding/wishlist.blade.php
@@ -6,8 +6,8 @@
 <div class="col-lg-8 divcenter bottommargin-lg">
     <ul class="skills">
         @foreach($wishlist_items as $items)
-            <li data-percent="80">
-                <span>{{ $items }}</span>
+            <li data-percent="0">
+                <span>&nbsp;&nbsp;&nbsp;&nbsp;{{ $items }}</span>
                 <div class="progress skills-animated divprogress">
                     <div class="progress-percent"><div class="counter counter-inherit counter-instant"><span data-from="0" data-to="0" data-refresh-interval="30" data-speed="1100">0</span>%</div></div>
                 </div>
