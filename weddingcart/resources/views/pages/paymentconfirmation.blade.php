e435120c13732eb183b4ba353ef3cdc6093a3798
Rajan Singh Rawat <rmprajan786@gmail.com>
2016-05-05 14:21:16 +0530

    payment confirmation module is done

---
+++ weddingcart/resources/views/pages/paymentconfirmation.blade.php
@@ -0,0 +1,93 @@
+  @extends('app')
+
+  @section('content')
+    <section id="content" class="secstyle">
+
+      <div class="content-wrap">
+
+        <div class="container clearfix">
+                
+       
+          <div class="heading-block center">
+            <h2>Contribute</h2>
+            
+          </div>
+                    
+          <div id="posts" class="events small-thumbs">
+              
+               <div class="entry clearfix">
+                    
+                    <div class="col-md-2">
+                    
+                       <div class="entry-title">
+                          
+                            <center><label>Product Name </label>
+                            <div>{{ $productDetails['product_name'] }}</div>
+                            </center>
+                        <!--    <input required aria-required="true" class="required form-control" id="productName" name="productName" placeholder="Product Name" type="text" value="{{ $productDetails['product_name'] }}"> -->
+                        </div>
+                          <div class="clear"></div>
+                                 
+                            <img src="{{ asset('../uploads/Products/' .$productDetails['product_image'] ) }}" alt="Product_Image" id="productImage" name="productImage" required>
+                            <br>
+                            <div>
+                            <center>
+                            <label style="margin-top: 10px ">Your Gift Amount : </label>
+                            &#8377 {{ $productDetails['contribution_amount'] }}
+                            
+                            </center>
+                            </div>
+                                
+                      </div>
+                      
+                      <div class="col-md-9">
+                          <div class="quick-contact-widget clearfix">
+                              <div class="input-group col_two_third">
+                                    <center><label>Product Description</label>
+                                    <div>{{ $productDetails['product_description'] }}</div>  
+                                    </center>
+                            <!--        <input required aria-required="true" class="required form-control" id="productDescription" name="productDescription" placeholder="Item Description" type="text" value="{{ $productDetails['product_description'] }}"> -->
+                              </div>
+                              <div class="input-group col_one_third col_last">
+                                    <center><label>Product Price</label>
+                                    <div>{{ $productDetails['product_price'] }} Rs.</div>
+                                    </center>
+                            <!--        <input required aria-required="true" class="required form-control email" id="productPrice" name="productPrice" placeholder="Contribute Amount" type="text" value="{{ $productDetails['product_price'] }}">  -->
+                              </div>
+                              	<div class="row">
+                              	<div class="col-md-3">
+                              	 Message : 
+                              	 </div>
+                                 <div class="col-md-9 div-for-message"> 
+                                    {{ $productDetails['message'] }}
+                                 </div>
+                                 </div>
+                                 
+                                 <div class="row">
+                                 <div class="col-md-3">
+                                 Your Message :
+                                 </div> 
+                                 <div class="col-md-9 div-for-message"> 
+                                 {{ $productDetails['guest_message'] }}
+                                 </div>
+                                 </div>
+                                <center>
+                                
+                                <a href="" class="btn btn-success">Confirm</a>
+                                <a href="" class="btn btn-success">Back</a>
+                                </center>
+                          </div>
+                        
+                             
+                        </div>
+              </div>
+             
+          </div>
+        </div> 
+        </form> 
+      </div>
+   </section>
+
+  
+
+ @stop
