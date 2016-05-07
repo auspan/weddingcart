cf5b05359ada6f7f6d6442cc1c7e4475bfdcd5f3
Utkal <utkal.pande@gmail.com>
2016-05-07 18:41:15 +0530

    Display About Bride and About Groom in the wedding landing page

--- weddingcart/resources/views/wedding/couple.blade.php
+++ weddingcart/resources/views/wedding/couple.blade.php
@@ -10,11 +10,7 @@
         </div>
         <div class="team-desc">
             <div class="team-title"><h4>{{ $gnm }}</h4><span>Groom</span></div>
-            <div class="team-content">Lorem ipsum dolor sit amet,
-                consectetur adipisicing elit. Commodi, pariatur, magni! Omnis reiciendis
-                architecto, cupiditate fuga dolores nam accusamus iste molestias quos
-                mollitia totam eius porro culpa incidunt, sunt rerum molestiae aliquid
-                non hic.</div>
+            <div class="team-content">{{ $gab }}</div>
             <div class="line topmargin-sm nobottommargin"></div>
             <a href="#" class="social-icon si-borderless si-small si-facebook" title="Facebook">
                 <i class="icon-facebook"></i>
@@ -39,10 +35,7 @@
         </div>
         <div class="team-desc">
             <div class="team-title"><h4>{{ $bnm }}</h4><span>Bride</span></div>
-            <div class="team-content">Blanditiis adipisci laudantium
-                reiciendis distinctio, molestiae, illum. Aut eveniet assumenda expedita
-                labore nulla commodi numquam perspiciatis, amet doloribus cum sint,
-                quisquam possimus eos aspernatur distinctio similique perferendis.</div>
+            <div class="team-content">{{ $bab }}</div>
             <div class="line topmargin-sm nobottommargin"></div>
             <a href="#" class="social-icon si-borderless si-small si-facebook" title="Facebook">
                 <i class="icon-facebook"></i>
