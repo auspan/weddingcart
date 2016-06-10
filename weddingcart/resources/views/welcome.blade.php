613b9235ffb503a8a43532c5caf40bff3c9ce0cd
Utkal <utkal.pande@gmail.com>
2016-06-11 00:57:23 +0530

    Corrected Welcome Page slider layout

--- weddingcart/resources/views/welcome.blade.php
+++ weddingcart/resources/views/welcome.blade.php
@@ -1,29 +1,9 @@
-@extends('app')
+@extends('home')
 
 @section('content')       
         
         @include('welcome.header')
-        <section id="slider" class="slider-parallax swiper_wrapper full-screen clearfix">
-            @include('welcome.slider') 
-        </section>        
-        
-        <!-- Content
-        ============================================= -->
-        <section id="content" class="sectionmar">
+        @include('welcome.slider')
 
-            <div class="content-wrap">
-            @include('welcome.infoLinks')
 
-                {{--<div class="section parallax dark nobottommargin divsecbackground" data-stellar-background-ratio="0.4">--}}
-                    {{--@include('welcome.clientSlider')--}}
-                {{--</div>--}}
-
-                {{--<div class="container clearfix divcontainer">--}}
-                    {{--@include('welcome.aboutTeam')--}}
-                {{--</div>--}}
-            </div>
-        </section><!-- #content end -->
-   </div><!-- #wrapper end -->
-</body>
-</html>
 @stop
