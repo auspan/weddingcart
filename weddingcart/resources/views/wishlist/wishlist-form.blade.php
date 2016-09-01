915b9e1c07d4d3b05a45613f70a1f8e65766d7bd
Utkal <utkal.pande@gmail.com>
2016-09-01 18:21:28 +0530

    Swapped Add More and Done (Back renamed) Buttons

--- weddingcart/resources/views/wishlist/wishlist-form.blade.php
+++ weddingcart/resources/views/wishlist/wishlist-form.blade.php
@@ -2,29 +2,31 @@
 
 @section('content')
 
-<meta name="_token" content="{{ csrf_token() }}">
     <section id="content" class="secstyle">
-      <div class="content-wrap">
-        <div class="container clearfix">
-          <div class="heading-block center">
-            <h2>Wishlist</h2>
-          </div>
-          <div id="posts" class="events small-thumbs">
-            <?php $count=1 ?>
-              @include('wishlist.products')
-          </div>
-             @include('wishlist.addProducts')
-      <div class="divider divider-center"><a href="#" data-scrollto="#header"><i class="icon-chevron-up"></i></a></div>
+        <div class="content-wrap">
+            <div class="container clearfix">
+                <div class="heading-block center">
+                    <h2>Wishlist</h2>
+                </div>
+                <div id="posts" class="events small-thumbs">
+                    <?php $count = 1 ?>
+                    @include('wishlist.products')
+                </div>
+                @include('wishlist.addProducts')
+                <div class="divider divider-center"><a href="#" data-scrollto="#header"><i class="icon-chevron-up"></i></a>
+                </div>
 
-      <div class="center bottommargin-lg">
-      
-        <a href="{{ url('/home') }}" class="button button-rounded button-xlarge">Back</a>
-          <div class="center">
-            <button class="button button-border button-rounded topmargin" data-toggle="modal" data-target=".bs-example-modal-sm">Add More</button>
-          </div>
-      </div>
-    </div>
-  </div>
-</section>
-     
-  @stop
+                <div class="center bottommargin-lg">
+                    <div class="center">
+                        <button class="button button-border button-rounded" data-toggle="modal"
+                                data-target=".bs-example-modal-sm">Add More
+                        </button>
+                    </div>
+                    <a href="{{ url('/home') }}" class="button button-rounded button-xlarge topmargin">Done</a>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+@stop
