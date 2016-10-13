ad487c162a6e743319fa05cece3d1f61beed7ad0
Utkal <utkal.pande@gmail.com>
2016-10-13 21:04:07 +0530

    Deleted commented code

--- weddingcart/resources/views/contacts/contacts.blade.php
+++ weddingcart/resources/views/contacts/contacts.blade.php
@@ -30,46 +30,7 @@
                         @endforeach
                 </ul>
                 <div class="clear"></div>
-                <!--<table id="myTable" class="table" cellspacing="0" width="100%">
-                    <thead>
-                        <tr>
-                            <th class="hidden"></th>
-                            <th>
-                                <div class="checkbox-inline">
-                                    <label>
-                                        <input type="checkbox" id="checkAll" name="query_myTextEditBox">
-                                    </label>
-                                </div>
-                            </th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Phone</th>
-                            <th>
-                                <button id="addSelected" type="button" class="btn-add btn addSelected" aria-label="Add">
-                                    <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                                </button>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $i=1 ?>
-                        @foreach($people as $person)
-                            <tr id="row{{$i}}" class="hideRow">
-                                <td id="id{{$i}}" class="hidden">{{ $person['googleId'] }}</td>
-                                <td scope="row"><input type="checkbox" id="checkbox-{{ $i }}" name="googleContacts"></td>
-                                <td id="name{{$i}}">{{ $person['name']}}</td>
-                                <td id="email{{$i}}">{{ $person['email'] }}</td>
-                                <td id="phone{{$i}}">{{ $person['phone'] }}</td>
-                                <td>
-                                    <button id="add-{{$i}}" type="button" class="btn-add btn addRow" aria-label="Add">
-                                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                                    </button>
-                                </td>
-                            </tr>
-                            <?php  $i++; ?>
-                        @endforeach
-                    </tbody>
-                </table>-->
+                
             </div>
         </div>
     </div>
